9d56fcc40d251ec021f1dca070fedd1353ee87fb
Вячеслав Рыжков <rugrisser@gmail.com>
2020-05-28 19:13:09 +0300

    Server Initialization

---
+++ server/src/main/java/edu/mshp/ideastore/IdeastoreApplication.java
@@ -0,0 +1,13 @@
+package edu.mshp.ideastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeastoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeastoreApplication.class, args);
+    }
+
+}
